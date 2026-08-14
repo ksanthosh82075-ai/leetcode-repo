db688e9f5ba08bba9a842b7cae5b12de91d8007a
ksanthosh82075-ai <ksanthosh82075@gmail.com>
2026-08-14 14:33:11 +0530

    Create 1452-RestaurantGrowth

---
+++ 1452-RestaurantGrowth/1452-RestaurantGrowth.sql
@@ -0,0 +1,21 @@
+-- Last updated: 8/14/2026, 2:33:21 PM
+# Write your MySQL query statement below
+SELECT
+    visited_on,
+    SUM(amount) OVER (
+        ORDER BY visited_on
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    ROUND(
+        AVG(amount) OVER (
+            ORDER BY visited_on
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ),
+        2
+    ) AS average_amount
+FROM (
+    SELECT visited_on, SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+) c
+LIMIT 18446744073709551615 OFFSET 6;
