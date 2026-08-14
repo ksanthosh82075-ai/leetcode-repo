7e823bdb9544e3e8eeed76ed954dbf02bdaad446
ksanthosh82075-ai <ksanthosh82075@gmail.com>
2026-08-14 14:36:47 +0530

    Create 206-ReverseLinkedList

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -0,0 +1,16 @@
+// Last updated: 8/14/2026, 2:36:58 PM
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
