6619f6e30094bbcce2e0af3f02c97efe75cd8cd0
ksanthosh82075-ai <ksanthosh82075@gmail.com>
2026-08-14 14:35:15 +0530

    Create 525-ContiguousArray

---
+++ 525-ContiguousArray/525-ContiguousArray.java
@@ -0,0 +1,28 @@
+// Last updated: 8/14/2026, 2:35:25 PM
+import java.util.*;
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int sum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                sum--;
+            } else {
+                sum++;
+            }
+
+            if (map.containsKey(sum)) {
+                maxLength = Math.max(maxLength, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return maxLength;
+    }
+}
