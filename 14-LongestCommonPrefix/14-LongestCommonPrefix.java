0e0aca6786ba1d8953d1d65c9f122eb74551821c
ksanthosh82075-ai <ksanthosh82075@gmail.com>
2026-08-14 14:39:03 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+// Last updated: 8/14/2026, 2:39:13 PM
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+}
