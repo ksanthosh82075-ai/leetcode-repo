71b3b8e5105f81c796d21275385d55d6f2ac19c1
ksanthosh82075-ai <ksanthosh82075@gmail.com>
2026-08-14 14:38:43 +0530

    Create 58-LengthOfLastWord

---
+++ 58-LengthOfLastWord/58-LengthOfLastWord.java
@@ -0,0 +1,20 @@
+// Last updated: 8/14/2026, 2:38:53 PM
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+        int length = 0;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count characters of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
