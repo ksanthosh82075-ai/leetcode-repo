5a314b2f7440d641177f73193abfec61e2b30577
ksanthosh82075-ai <ksanthosh82075@gmail.com>
2026-08-14 14:33:57 +0530

    Create 958-SortArrayByParityIi

---
+++ 958-SortArrayByParityIi/958-SortArrayByParityIi.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/14/2026, 2:34:07 PM
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int i = 0, j = 1, n = nums.size();
+        
+        while (i < n && j < n) {
+            if (nums[i] % 2 == 0) i += 2;
+            else if (nums[j] % 2 == 1) j += 2;
+            else {
+                swap(nums[i], nums[j]);
+                i += 2;
+                j += 2;
+            }
+        }
+        return nums;
+    }
+};
